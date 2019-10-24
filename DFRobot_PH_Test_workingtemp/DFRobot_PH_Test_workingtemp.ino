c9e59c8b8d14a5eeff8c7665654a840a758a1028
aaronwash99 <55113157+aaronwash99@users.noreply.github.com>
2019-10-24 01:59:33 -0400

    Add files via upload
    These files will help you test your Gravity pH and EC sensors with the DHT11 Temperature sensor

---
+++ DFRobot_PH_Test_workingtemp/DFRobot_PH_Test_workingtemp.ino
@@ -0,0 +1,72 @@
+/*
+ * file DFRobot_PH.ino
+ * @ https://github.com/DFRobot/DFRobot_PH
+ *
+ * This is the sample code for Gravity: Analog pH Sensor / Meter Kit V2, SKU:SEN0161-V2
+ * In order to guarantee precision, a temperature sensor such as DS18B20 is needed, to execute automatic temperature compensation.
+ * You can send commands in the serial monitor to execute the calibration.
+ * Serial Commands:
+ *   enterph -> enter the calibration mode
+ *   calph   -> calibrate with the standard buffer solution, two buffer solutions(4.0 and 7.0) will be automaticlly recognized
+ *   exitph  -> save the calibrated parameters and exit from calibration mode
+ *
+ * Copyright   [DFRobot](http://www.dfrobot.com), 2018
+ * Copyright   GNU Lesser General Public License
+ *
+ * version  V1.0
+ * date  2018-04
+ */
+
+#include "DFRobot_PH.h"
+#include <EEPROM.h>
+#include <dht11.h>
+dht11 DHT;
+
+#define DHT11_PIN A0
+#define PH_PIN A1
+float voltage,phValue,temperature = 25;
+DFRobot_PH ph;
+
+void setup()
+{
+    Serial.begin(115200);  
+    ph.begin();
+    Serial.begin(9600);
+
+}
+
+void loop()
+{
+    static unsigned long timepoint = millis();
+    if(millis()-timepoint>1000U){                  //time interval: 1s
+        timepoint = millis();
+        temperature = readTemperature();         // read your temperature sensor to execute temperature compensation
+        voltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
+        phValue = ph.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
+        Serial.print("temperature:");
+        Serial.print(DHT.temperature * 1.8 +32);
+        Serial.print("^C  pH:");
+        Serial.println(phValue,2);
+    }
+    ph.calibration(voltage,temperature);           // calibration process by Serail CMD
+}
+
+float readTemperature()
+{
+ int chk = DHT.read(DHT11_PIN);    // READ DATA= DHT.read(DHT11_PIN);    // READ DATA
+  Serial.print("DHT11, \t");
+  switch (chk){
+    case DHTLIB_OK:  
+                Serial.print("OK,\n"); 
+                break;
+    case DHTLIB_ERROR_CHECKSUM: 
+                Serial.print("Checksum error,\t"); 
+                break;
+    case DHTLIB_ERROR_TIMEOUT: 
+                Serial.print("Time out error,\t"); 
+                break;
+    default: 
+                Serial.print("Unknown error,\t"); 
+                break;
+  }
+  }
